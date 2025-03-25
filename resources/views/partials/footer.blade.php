430a4b540a368807e7e3e65429d5e4791457e1c8
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-03-25 22:58:38 +0100

    fix and completed header and footer section

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,39 @@
 <footer>
+    
+    <div class="container-fluid footer-up d-flex align-items-center">
+        <div class="container">
+            <ul class="container d-flex justify-content-evenly footer-up-ul-class">
+                <li class="d-flex align-items-center">
+                    <i><img
+                            src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}"
+                            alt="Digital Comics"></i><span>Digital Comics</span>
+                </li>
+                <li class="d-flex align-items-center">
+                    <i class="d-flex"><img
+                            src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}"
+                            alt="Digital Comics"></i><span>DC Merchandise</span>
+                </li>
+                <li class="d-flex align-items-center">
+                    <i class="d-flex"><img
+                            src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}"
+                            alt="Digital Comics"></i><span>Subscription</span>
+                </li>
+                <li class="d-flex align-items-center">
+                    <i class="d-flex"><img
+                            src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}"
+                            alt="Digital Comics"></i><span>Comic Shop Locator</span>
+                </li>
+                <li class="d-flex align-items-center">
+                    <i class="d-flex"><img
+                            src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}"
+                            alt="Digital Comics"></i><span>DC Power Visa</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    
+    
     <div class="container">
         <div class="row d-flex">
             <div class="col-6 col-sm-4 col-md-3 col-lg-2">
@@ -46,14 +81,29 @@
                     <li>DC Power Visa</li>
                 </ul>
             </div>
-            <div class="col-6 col-sm-6 col-md-3 col-lg-5">
+            <div class="col-6 col-sm-6 col-md-3 col-lg-5 container-dc-image">
                 {{-- Con l'istruzione di seguito per accedere all'immagine da inserire nel layout, affinché Blade la processi, utilizzo la direttiva
          Vite::asset() come di seguito (notiamo come asset sia un metodo statico): --}}
-                <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="Logo DC Comics">
-            </div>
-            <div class="row">
-
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Logo DC Comics" class="dc-image">
             </div>
         </div>
     </div>
+
+    
+    
+    <div class="container-fluid footer-down d-flex">
+        <div class="container d-flex justify-content-between align-items-center">
+            <button class="button-footer-down">Sign-Up Now!</button>
+
+            <ul class="d-flex justify-content-between align-items-center button-footer-social">
+                <li>Follow Us</li>
+                <li><i><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook"></i></li>
+                <li><i><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter"></i></li>
+                <li><i><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="Youtube"></i></li>
+                <li><i><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest"></i></li>
+                <li><i><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope"></i></li>
+            </ul>
+        </div>
+    </div>
+
 </footer>
